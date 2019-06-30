010b947817a92a78642f794f77244efd1466b43b
Merijn Hendriks <innohurrytocode@gmail.com>
2019-06-30 16:20:29 +0200

    Split transparant layer from background

--- Project/Properties/Resources.Designer.cs
+++ Project/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Launcher.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap bg_alpha {
+            get {
+                object obj = ResourceManager.GetObject("bg_alpha", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap bg_error {
             get {
                 object obj = ResourceManager.GetObject("bg_error", resourceCulture);
