316ae87777a461d3b1a87343f5a8479a54ab0af7
Merijn Hendriks <innohurrytocode@gmail.com>
2019-07-02 17:29:40 +0200

    Add functionality to menu bar

--- Project/Views/MainWindow.xaml.cs
+++ Project/Views/MainWindow.xaml.cs
@@ -10,61 +10,61 @@ namespace Launcher
     /// </summary>
     public partial class MainWindow : Window
     {
-        private LauncherSettings settings = new LauncherSettings();
+        private LauncherSettings laucherSettings = new LauncherSettings();
 
         public MainWindow()
         {
             InitializeComponent();
-            LoadSettings();
+            LoadAllSettings();
+            OnAccount(null, null);
         }
 
-        private void LoadSettings()
+        private void HideAllMenuBarGrids()
         {
-            Email.Text = settings.GetEmail();
-            Password.Text = settings.GetPassword();
-            GameLocation.Text = settings.GetGameLocation();
-            ServerLocation.Text = settings.GetServerLocation();
-            BackendURL.Text = settings.GetBackendURL();
+            // account
+            LoginGrid.Visibility = Visibility.Hidden;
+            // RegisterGrid.Visibility = Visibility.Hidden;
+            // AccountGrid.Visibility = Visibility.Hidden;
+            // ChangeEmailGrid.Visibility = Visibility.Hidden;
+            // ChangePasswordGrid.Visibility = Visibility.Hidden;
+
+            // server
+            // ServerGeneral.Visibility = Visibility.Hidden;
+            // ServerBots.Visibility = Visibility.Hidden;
+            // ServerWeather.Visibility = Visibility.Hidden;
+
+            // launcher
+            SettingsGrid.Visibility = Visibility.Hidden;
         }
 
-        private void OnStartGame(object sender, RoutedEventArgs e)
+        #region LOAD_SETTINGS
+        private void LoadAccountSettings()
         {
-            GameStarter starter = new GameStarter(GameLocation.Text, BackendURL.Text, Email.Text, Password.Text);
+            Email.Text = laucherSettings.GetEmail();
+            Password.Text = laucherSettings.GetPassword();
+            ClientBackendURL.Text = laucherSettings.GetBackendURL();
         }
 
-        private void OnStartServer(object sender, RoutedEventArgs e)
+        private void LoadLauncherSettings()
         {
-            ServerStarter starter = new ServerStarter(ServerLocation.Text);
+            GameLocation.Text = laucherSettings.GetGameLocation();
+            ServerLocation.Text = laucherSettings.GetServerLocation();
         }
 
-        private void OnChangeEmail(object sender, TextChangedEventArgs e)
+        private void LoadAllSettings()
         {
-            settings.SetEmail(Email.Text);
+            LoadAccountSettings();
+            LoadLauncherSettings();
         }
+        #endregion
 
-        private void OnChangePassword(object sender, TextChangedEventArgs e)
-        {
-            settings.SetPassword(Password.Text);
-        }
-
-        private void OnChangeGameLocation(object sender, TextChangedEventArgs e)
-        {
-            settings.SetGameLocation(GameLocation.Text);
-        }
-
-        private void OnChangeServerLocation(object sender, TextChangedEventArgs e)
-        {
-            settings.SetServerLocation(ServerLocation.Text);
-        }
-
-        private void OnChangeBackendURL(object sender, TextChangedEventArgs e)
-        {
-            settings.SetBackendURL(BackendURL.Text);
-        }
-
+        #region MENU_BAR
+        // TODO: Check if user is logged in
         private void OnAccount(object sender, RoutedEventArgs e)
         {
-            // code here
+            HideAllMenuBarGrids();
+            LoginGrid.Visibility = Visibility.Visible;
+            LoadAccountSettings();
         }
 
         private void OnServerGeneral(object sender, RoutedEventArgs e)
@@ -84,7 +84,61 @@ namespace Launcher
 
         private void OnSettings(object sender, RoutedEventArgs e)
         {
+            HideAllMenuBarGrids();
+            SettingsGrid.Visibility = Visibility.Visible;
+            LoadLauncherSettings();
+        }
+        #endregion
+
+        #region APPLICATION_LAUNCHER
+        private void OnStartGame(object sender, RoutedEventArgs e)
+        {
+            GameStarter starter = new GameStarter(GameLocation.Text, ClientBackendURL.Text, Email.Text, Password.Text);
+        }
+
+        private void OnStartServer(object sender, RoutedEventArgs e)
+        {
+            ServerStarter starter = new ServerStarter(ServerLocation.Text);
+        }
+        #endregion
+
+        #region ACCOUNT_LOGIN
+        private void OnChangeEmail(object sender, TextChangedEventArgs e)
+        {
+            laucherSettings.SetEmail(Email.Text);
+        }
+
+        private void OnChangePassword(object sender, TextChangedEventArgs e)
+        {
+            laucherSettings.SetPassword(Password.Text);
+        }
+
+        private void OnChangeClientBackendURL(object sender, TextChangedEventArgs e)
+        {
+            laucherSettings.SetBackendURL(ClientBackendURL.Text);
+        }
+
+        private void OnLogin(object sender, RoutedEventArgs e)
+        {
             // code here
         }
+
+        private void OnRegisterMenu(object sender, RoutedEventArgs e)
+        {
+            // code here
+        }
+        #endregion
+
+        #region LAUNCHER_SETTINGS
+        private void OnChangeGameLocation(object sender, TextChangedEventArgs e)
+        {
+            laucherSettings.SetGameLocation(GameLocation.Text);
+        }
+
+        private void OnChangeServerLocation(object sender, TextChangedEventArgs e)
+        {
+            laucherSettings.SetServerLocation(ServerLocation.Text);
+        }
+        #endregion
     }
 }
